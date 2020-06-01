8dae2c7fc410f15b115db9c651ab891115ac14b3
EvilTeliportist <44454017+EvilTeliportist@users.noreply.github.com>
2020-05-31 20:05:29 -0400

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -1,76 +1,74 @@
-import os
-import matplotlib.pyplot as plt
-from Robinhood import Robinhood
-import numpy
+import pynet
+import os, numpy
 import yfinance as yf
-
-def get_data(name):
-    
-    ticker = yf.Ticker(name)
-    hist = ticker.history(period='max')
-    hist2 = [i[1] for i in hist.to_numpy()]
-    return hist2
-    
-
-def make_ema(data, smoothing):
-    ema = [data[0]]
-
-    for i in range(len(data))[1:]:
-        ema.append((smoothing*data[i]) + ((1 - smoothing) * ema[-1]))
-
-    return ema
-
-def find_periods(ema, dema, neg_thresh):
-    above = True
-    points = []
-    for i in range(len(ema)):
-        if (ema[i] < dema[i]) == above:
-            points.append(i)
-            above = not above
-
-    points = points[::2]
-
-    slope = []
-    last = 0
-    for i in range(len(dema)):
-        if dema[i] < (last - neg_thresh):
-            slope.append(i-1)
-        last = dema[i]
-
-    return points, slope
-
-def cutoff(index, neg):
-    for i in neg:
-        if i > index:
-            return i
-
-def make_graph(name, day_thresh, neg_thresh):
-    clean = get_data(name)#'C:/Users/brhou/Desktop/trading_bot/BackTesting/historical_data/' + name)
-    data = clean#[float(i[4]) for i in clean[1:]]
-
-    #higher smoothness less smooth
-    ema = make_ema(data, .1)
-    dema = make_ema(data, .25)
-    buy, slope = find_periods(ema, dema, neg_thresh)
-    spans = [[i, cutoff(i, slope)] for i in buy]
-
-    plt.plot(data)
-    plt.plot(ema)
-    plt.plot(dema)
+import matplotlib.pyplot as plt
 
 
-    profits = 0
-    for i in spans:
-        try:
-            if i[1] - i[0] > day_thresh:
-                plt.axvspan(i[0], i[1]+1, color = 'red', alpha = .5)
-                profits += (data[i[1]] - data[i[0]])
-        except:
-            pass
+#get initial data
+ticker = yf.Ticker('MSFt')
+raw = [i[1] for i in ticker.history(period='max').to_numpy()]
 
-    print('Total Proft: $' + str(round(profits, 2)) + ' or ' + str(round(profits/data[0], 2))
-          + '%\nAnnualized Return: $' + str(round(profits/(len(data)/253), 2)))
 
-    plt.show()
+# split raw into sets of 5 days to train the network with
+c = 0
+split_raw = []
+temp = []
+for i in raw:
+    if c < 5:
+        temp.append(i)
+        c += 1
+    else:
+        split_raw.append(temp)
+        temp = []
+        c = 0
 
-make_graph('TSLA', 0, -1 )
+
+# select only the second half of the data to get rid of very low prices that have large
+# percentage based variation (ex $.06 and $.07 vs $22.50 and $22.51)
+split_raw = split_raw[len(split_raw)//2:]
+
+
+# normalize each set of data, preparing for input into the neural network
+split_raw = [(set/numpy.linalg.norm(numpy.asarray(set))).tolist() for set in split_raw]
+
+
+# create an array of expected outputs to train the network with
+# expected outputs of 1 represent a good buy, while outputs of
+# 0 represent a bad buy
+exp_out = []
+for set in split_raw:
+    if set[-1] > set[-2]:
+        exp_out.append([1])
+    else:
+        exp_out.append([0])
+
+test_out = numpy.array(exp_out[len(exp_out)//2:])
+exp_out = numpy.array(exp_out[:len(exp_out)//2])
+
+
+# remove the last datapoint from each set because they are acting
+# as the "answers" and we should not train the network on data that
+# we wouldn't necissarily have access to during implementation
+split_raw = [set[:-1] for set in split_raw]
+split_raw = numpy.array(split_raw)
+
+# creates a testing and training dataset by chopping the data in half
+training = split_raw[:len(split_raw)//2]
+testing = split_raw[len(split_raw)//2:]
+
+
+# initializing neural network class
+network = pynet.NeuralNetwork(training, exp_out, 2, 2, .0001)
+network.print_while_training = True;
+
+# train
+network.train(1000)
+
+# show a plot of error over time
+plt.plot(network.all_error)
+plt.show()
+
+# test by inputting the test set into
+test_output = network.test(testing)
+average_test_error = sum(test_output - test_out)/len(test_out)
+print("Average Test Error: " + str(round(100*average_test_error[0], 4)) + "%")
